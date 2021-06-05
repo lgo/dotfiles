57aea60930644fa2313c60a7f62ca0cee13cd434
Joey Pereira <joey@pereira.io>
2021-06-05 18:56:55 -0400

    bits

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -5,6 +5,9 @@ export PATH="usr/local/opt/ccache/libexec:$PATH"
 ## VS Code standard location.
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 ## Java installed by Brew.
-export PATH="/usr/local/opt/openjdk/bin:$PATH"
+# export PATH="/usr/local/opt/openjdk/bin:$PATH"
+## Java installed by Amazon
+export PATH="/Library/Java/JavaVirtualMachines/amazon-corretto-11.jdk/Contents/Home/bin:$PATH"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/amazon-corretto-11.jdk/Contents/Home"
 ## User binaries.
 export PATH="~/.bin:$PATH"
