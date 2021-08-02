33492966038b889671332892a1604cc87566285a
Joey Pereira <joey@pereira.io>
2021-08-02 08:18:01 -0400

    speed up evals

--- zsh/brew/fpath.zsh
+++ zsh/brew/fpath.zsh
@@ -1,5 +1,7 @@
 # Include all of brew's autocompletion suggestions.
 # This includes some common tools, such as git.
 if type brew &>/dev/null; then
-  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+  # TODO(joey): brew --prefix hardcoded.
+  # FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+  FPATH=/usr/local//share/zsh/site-functions:$FPATH
 fi
--- zsh/system/grc.zsh
+++ zsh/system/grc.zsh
@@ -1,5 +1,6 @@
 # GRC colorizes nifty unix tools all over the place
-if (( $+commands[grc] )) && (( $+commands[brew] ))
-then
-  source `brew --prefix`/etc/grc.bashrc
+if (( $+commands[grc] )) && (( $+commands[brew] )); then
+  # TODO(joey): brew --prefix hardcoded.
+  # source `brew --prefix`/etc/grc.bashrc
+  source /usr/local/etc/grc.bashrc
 fi
